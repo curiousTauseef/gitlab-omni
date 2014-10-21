ee383e848d762ab2e8cb817feb092c5f927fdba6
Sebastian Grewe <sebastian@grewe.ca>
2014-10-21 13:53:30 +0200

    [VERSION] 1.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sebgrewe@bigpoint.net'
 license          'Apache 2.0'
 description      'Installs and configures a default Gitlab instance using their Omnibus Package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.3.0'
+version           '1.3.1'
 
 supports 'debian', '>= 7.0'
 supports 'centos', '>= 6.5'
